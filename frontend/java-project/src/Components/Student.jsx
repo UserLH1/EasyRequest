fce11c19f1d6206e236549564d32415316579f39
UserLH1 <lazeahoratiu@gmail.com>
2024-01-08 15:57:54 +0200

    added some styles to form

--- frontend/java-project/src/Components/Student.jsx
+++ frontend/java-project/src/Components/Student.jsx
@@ -1,18 +1,19 @@
 import { Button, Container, Paper } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import * as React from "react";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
   const [name, setName] = React.useState("");
   const [adress, setAdress] = React.useState("");
   const [students, setStudents] = React.useState([]);
+
   const handleClick = (e) => {
     e.preventDefault();
-
     const student = { name, adress };
-    console.log(student);
 
     fetch("http://localhost:8080/student/add", {
       method: "POST",
@@ -21,11 +22,14 @@ export default function Student() {
     })
       .then(() => {
         console.log("New Student added");
+        toast.success("Thank you for registering!"); // Display success toast
       })
       .catch((error) => {
         console.error("Error adding student:", error);
+        toast.error("Failed to register."); // Display error toast
       });
   };
+
   React.useEffect(() => {
     fetch("http://localhost:8080/student/getAll")
       .then((res) => res.json())
@@ -36,6 +40,7 @@ export default function Student() {
 
   return (
     <Container>
+      <ToastContainer />
       <Paper elevation={3} style={paperStyle}>
         <h1 style={{ color: "blue" }}>
           <u>Add Student</u>
